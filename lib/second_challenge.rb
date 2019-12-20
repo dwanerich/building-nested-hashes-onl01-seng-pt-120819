9f88ee69553c97b352955b159618da1f79db1c98
dwane richards <dwanerich@gmail.com>
2019-12-20 02:17:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,22 +1,5 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
-        ]
-      }, 
-    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-      }
-      }}
+  
 
   
 end
